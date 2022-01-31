a199da81879cf3d91295eabe654ae6201956bc3e
Shayan Talebian <shayantalebianwork@gmail.com>
2022-01-31 14:50:13 +0330

    Finish check button actions

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 "use strict";
 
+// Craete that random number
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector(".number").textContent = secretNumber;
 
+// Add action to check button
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
@@ -15,5 +17,9 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "⛔ Not valid!";
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "📈 Too high!";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "📉 Too Low!";
   }
 });
