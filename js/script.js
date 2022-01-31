e6bf981ad64363ee0188e540ef0ce2369ee625cc
Shayan Talebian <shayantalebianwork@gmail.com>
2022-01-31 15:10:42 +0330

    Create score

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector(".number").textContent = secretNumber;
 
+let score = 20; // Score
+
 // Add action to check button
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -18,13 +20,27 @@ document.querySelector(".check").addEventListener("click", function () {
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
   } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "📈 Too high!";
-  } else if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "📉 Too Low!";
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess <= secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too Low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
 
 // Create guide alert
-function message() {
-  alert("To have better exprince visit this website with your laptop ");
-}
+// function message() {
+//   alert("To have better exprince visit this website with your laptop ");
+// }
